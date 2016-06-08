40c24e600ca71e39818a6ce16b7ed86b2a9ea507
UltimaN3rd <UltimaN3rd@gmail.com>
2016-06-08 06:53:08 -0500

    Finished lvl e1, completed save system, changed some gfx
    Changed wall, boulder, door and switch images. Game now saves after any level is completed.

--- objects/stairs/stairs.gd
+++ objects/stairs/stairs.gd
@@ -1,11 +1,34 @@
 
 extends Area2D
 
-export(String) var target_scene = "res://scenes/levelselect.tscn"
+export(int) var this_level = 0
+
+var save_file = File.new()
+var level_complete = [false, false, false, false, false, false] # B1/2, I1/2, E1/2
 
 func _ready():
-	pass
+	if(save_file.file_exists("user://save.txt")):
+		save_file.open("user://save.txt", save_file.READ)
+		var save_data = save_file.get_as_text()
+		if(not save_data.empty()):
+			for i in range(level_complete.size()):
+				level_complete[i] = (save_data[i] == "1")
+	else:
+		save_file.open("user://save.txt", save_file.WRITE)
+		for i in range(level_complete.size()):
+			save_file.store_var(false)
+	save_file.close()
 
 func _on_Stairs_body_enter( body ):
 	if(body.get_name() == "Player"):
-		get_tree().change_scene(target_scene)
+		level_complete[this_level] = true
+		
+		save_file.open("user://save.txt", save_file.WRITE)
+		for i in range(level_complete.size()):
+			if(level_complete[i]):
+				save_file.store_string("1")
+			else:
+				save_file.store_string("0")
+		save_file.close()
+		
+		get_tree().change_scene("res://scenes/levelselect.tscn")
